488fbf099f4f6d57ade61321fbf166997cff5d78
Ovisana <159209027+Ovisana@users.noreply.github.com>
2024-03-25 10:37:33 +0700

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('.fa-bars').click(function(){
+    $('.papa').click(function(){
         $(this).toggleClass('fa-times');
         $('.navbar').toggleClass('nav-toggle');
     });
